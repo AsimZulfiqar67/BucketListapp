78e1bce58a844d235b6e066174a52442d9666710
Asim Zulfiqar <32940052+AsimZulfiqar67@users.noreply.github.com>
2019-07-08 15:59:26 +0500

    asfa
    asda

---
+++ BucketList-master/app/src/main/java/com/example/asimz/bucketlist/db/TaskContract.java
@@ -0,0 +1,20 @@
+package com.example.asimz.bucketlist.db;
+
+import android.provider.BaseColumns;
+
+public class TaskContract {
+    public static final String DB_NAME = "com.example.asimz.db";
+    public static final int DB_VERSION = 30;
+
+    public class PointsEntry implements  BaseColumns
+        {
+            public static final String TABLE1 = "points";
+            public static final String COL_POINTS_TP = "totalPoints";
+        }
+
+    public class TaskEntry implements BaseColumns
+        {
+            public static final String TABLE = "tasks";
+            public static final String COL_TASK_TITLE = "title";
+        }
+}
